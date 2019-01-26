54461393993ad167ffd7dfe0ed918cf6df7deeee
Kevin Balz <kevin.balz@web.de>
2019-01-26 22:19:32 +0100

    Add upgrades

---
+++ LassMichInRuhGame/Assets/Kevin/Scripts/AmazenTrigger.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmazenTrigger : MonoBehaviour
+{
+    public GameObject store;
+    bool inside = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inside = false;
+            if (store.activeSelf)
+            {
+                store.SetActive(false);
+            }
+            
+        }
+    }
+
+    private void Update()
+    {
+        if (inside && Input.GetButtonDown("Jump"))
+        {
+            store.SetActive(!store.activeSelf);
+        }
+    }
+}
