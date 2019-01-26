f94d22fd6eac960eeeee07132c682f7cc64b6297
Niklas <niklas.burri@web.de>
2019-01-26 18:36:01 +0100

    Added Neighbours

--- LassMichInRuhGame/Assets/Code_N/Scripts/Enemys/Enemy.cs
+++ LassMichInRuhGame/Assets/Code_N/Scripts/Enemys/Enemy.cs
@@ -10,31 +10,87 @@ public class Enemy : MonoBehaviour
     private float health = 10;
 
     public UnityEvent OnDie;
+    public UnityEvent OnNeighbourDie;
+    public UnityEvent OnNeighbourReset;
 
     public GameObject[] deathObjects;
 
     public GameObject[] hitObject;
 
+    public bool Neighbour = false;
+    private bool flee = true;
+    public Transform grillPoint;
+    public float grillTime;
+    public ParticleSystem smokeParticle;
+
+    private void Start()
+    {
+        if(Neighbour)
+        {
+            StartWait();
+        }
+    }
+
+    public void Update()
+    {
+        if (Neighbour)
+        {
+            if (Vector3.Distance(grillPoint.position, transform.position) <= 0.5f)
+            {
+                grillTime += Time.deltaTime;
+                if (grillTime >= 3)
+                {
+                    smokeParticle.Play();
+                }
+            }
+        }
+    }
+
     public void TakeDamage(float damage)
     {
-        Debug.Log("Damage");
+        //Debug.Log("Damage");
+        if (Neighbour && flee)
+            return;
+
         health -= damage;
         if (health <= 0)
         {
-            foreach(GameObject obj in deathObjects)
+            if (Neighbour)
             {
-                Instantiate(obj, transform.position, transform.rotation);
+                OnNeighbourDie.Invoke();
+                flee = true;
+                smokeParticle.Stop();
+                grillTime = 0;
             }
-            OnDie.Invoke();
-            Destroy(gameObject);
+            else
+            {
+                foreach (GameObject obj in deathObjects)
+                {
+                    Instantiate(obj, transform.position, obj.transform.rotation);
+                }
+                OnDie.Invoke();
+                Destroy(gameObject);
+            }
         }
         else
         {
             foreach (GameObject obj in hitObject)
             {
-                Instantiate(obj, transform.position, transform.rotation);
+                Instantiate(obj, transform.position, obj.transform.rotation);
             }
         }
     }
   
+    public void StartWait()
+    {
+        StartCoroutine(ResetNeighbours());
+    }
+
+    IEnumerator ResetNeighbours()
+    {
+        yield return new WaitForSeconds(Random.Range(8, 10));
+        health = 10;
+        OnNeighbourReset.Invoke();
+        flee = false;
+    }
 }
