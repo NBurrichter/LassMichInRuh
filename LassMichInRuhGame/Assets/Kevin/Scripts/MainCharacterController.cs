3d43e32f77696efa06fa522657f5b4cc0a013ecc
Kevin Balz <kevin.balz@web.de>
2019-01-26 11:02:53 +0100

    Correct Character Movement

--- LassMichInRuhGame/Assets/Kevin/Scripts/MainCharacterController.cs
+++ LassMichInRuhGame/Assets/Kevin/Scripts/MainCharacterController.cs
@@ -7,7 +7,11 @@ public class MainCharacterController : MonoBehaviour
     public float speed = 1f;
     public float fireRate = 10;
     public GameObject bulletPrefab;
+    public Sprite leftSprite;
+    public Sprite upSprite;
+    public Sprite downSprite;
     private Collider col;
+    private SpriteRenderer renderer;
     RaycastHit[] hits = new RaycastHit[1024];
     Collider[] cols = new Collider[1024];
     float lastFire = 0;
@@ -17,75 +21,117 @@ public class MainCharacterController : MonoBehaviour
     private void Awake()
     {
         col = GetComponent<Collider>();
+        renderer = GetComponent<SpriteRenderer>();
     }
 
 
     private void Update()
     {
-        var axis = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        var axis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         var direction = axis.normalized;
-        var distance = Mathf.Min(1 , axis.magnitude) * speed;
+        var distance = Mathf.Min(1, axis.magnitude) * speed;
         var movement = direction * distance;
         var xLimit = direction.x > 0 ? Mathf.Infinity : -Mathf.Infinity;
 
-        var target = transform.position + movement;
+        if (movement.magnitude > 0)
+        {
+            AdjustOrientation(direction);
+        }
+        
 
         if (movement.x != 0)
         {
             for (var i = 0; i < castsPerCount; i++)
             {
-                //Debug.Log($"{col.bounds.max} {col.bounds.min}");
-                var originX = direction.x < 0 ? col.bounds.min.x : col.bounds.max.x;
-                var startY = col.bounds.max.y;
-                var endY = col.bounds.min.y;
-                var origin = new Vector3(originX, Mathf.Lerp(startY, endY, (float)i / (castsPerCount - 1)), transform.position.z);
-                var hitCount = Physics.RaycastNonAlloc(origin, direction, hits, distance);
-                Debug.DrawRay(origin, movement);
-                //Debug.Log($"{origin} {movement}");
+                var xBounds = direction.x < 0 ? col.bounds.min : col.bounds.max;
+                var originX = xBounds.x;
+                var startY = col.bounds.max.z;
+                var endY = col.bounds.min.z;
+                var rayDirection = direction;
+                var rayDistance = Mathf.Abs(direction.x * distance);
+                rayDirection.z = 0;
+                var origin = new Vector3(originX, transform.position.y, Mathf.Lerp(startY, endY, (float)i / (castsPerCount - 1)));
+                var hitCount = Physics.RaycastNonAlloc(origin, rayDirection, hits, rayDistance);
+                Debug.DrawLine(origin, origin + rayDirection * rayDistance);
                 for (var j = 0; j < hitCount; j++)
                 {
                     if (hits[j].transform == col.transform)
                     {
                         continue;
                     }
-                    if (direction.x > 0)
+
+                    GetComponent<SanityController>().RemoveSanity(1);
+                    var point = hits[j].point;
+                    var hitDistance = point.x - origin.x;
+                    if (Mathf.Abs(movement.x) > Mathf.Abs(hitDistance))
                     {
-                        xLimit = Mathf.Min(xLimit, hits[j].point.x);
+                        movement.x = hitDistance;
                     }
-                    else
-                    {
-                        xLimit = Mathf.Max(xLimit, hits[j].point.x);
-                    }
-                    Debug.Log($"{xLimit}");
                 }
             }
         }
-        /*
-        var hitCount = Physics.BoxCastNonAlloc(col.transform.position, col.bounds.extents/2, direction, hits, col.transform.rotation, movement);
-        for (var i = 0; i < hitCount; i++)
+
+        if (movement.z != 0)
         {
-            if (hits[i].collider == col)
+            for (var i = 0; i < castsPerCount; i++)
+            {
+                var xBounds = direction.z < 0 ? col.bounds.min : col.bounds.max;
+                var originX = xBounds.z;
+                var startY = col.bounds.max.x;
+                var endY = col.bounds.min.x;
+                var rayDirection = direction;
+                var rayDistance = Mathf.Abs(direction.z * distance);
+                rayDirection.x = 0;
+                var origin = new Vector3(Mathf.Lerp(startY, endY, (float)i / (castsPerCount - 1)), transform.position.y, originX);
+                var hitCount = Physics.RaycastNonAlloc(origin, rayDirection, hits, rayDistance);
+                Debug.DrawLine(origin, origin + rayDirection * rayDistance);
+                for (var j = 0; j < hitCount; j++)
+                {
+                    if (hits[j].transform == col.transform)
+                    {
+                        continue;
+                    }
+
+                    GetComponent<SanityController>().RemoveSanity(1);
+                    var point = hits[j].point;
+                    var hitDistance = point.z - origin.z;
+                    if (Mathf.Abs(movement.z) > Mathf.Abs(hitDistance))
+                    {
+                        movement.z = hitDistance;
+                    }
+                }
+            }
+        }
+
+        transform.position += movement;
+
+        var colCount = Physics.OverlapBoxNonAlloc(col.bounds.center, col.bounds.extents / 2, cols);
+        for (var i = 0; i < colCount; i++)
+        {
+            var other = cols[i];
+            if (other == col)
             {
                 continue;
             }
- 
-            movement = Mathf.Max(0, Mathf.Min(movement, Mathf.Abs( (hits[i].point - col.ClosestPointOnBounds(hits[i].point)).magnitude)));
+            Debug.Log("Overlap");
+            Vector3 trans;
+            float transDist;
+            Physics.ComputePenetration(col, transform.position, transform.rotation, other, other.transform.position, other.transform.rotation, out trans, out transDist);
+            transform.position += trans * transDist;
         }
-        */
 
-        target.x = direction.x > 0 ? Mathf.Min(xLimit, target.x) : Mathf.Max(xLimit, target.x);
-        transform.position = target;
-
+        var adjustOrientation = direction;
         if (Input.GetButton("Fire1"))
         {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var fireDirection = mousePos - transform.position;
             if (Time.time >= lastFire + (1/fireRate))
             {
-                var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                var fireDirection = mousePos - transform.position;
-                fireDirection.z = 0;
+                
+                fireDirection.y = 0;
                 fireDirection = fireDirection.normalized;
                 lastFire = Time.time;
-                var bullet = Instantiate(bulletPrefab, col.bounds.ClosestPoint(mousePos), Quaternion.identity);
+                var bullet = Instantiate(bulletPrefab, col.bounds.ClosestPoint(mousePos),Quaternion.Euler(90, 0, 0));
                 var controller = bullet.GetComponent<BulletController>();
                 var bulletCollider = bullet.GetComponent<Collider>();
                 Vector3 bullDirection;
@@ -98,6 +144,42 @@ public class MainCharacterController : MonoBehaviour
                 controller.speed = 1;
                 Destroy(bullet, 1);
             }
+            AdjustOrientation(fireDirection);
+        }
+
+    }
+
+    public void AdjustOrientation(Vector3 direction)
+    {
+        Debug.Log(direction);
+        if (direction.z == 0 && direction.x == 0)
+        {
+            return;
+        }
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+        {
+            renderer.sprite = leftSprite;
+            if (direction.x < 0)
+            {
+                renderer.flipX = false;
+            }
+            else if (direction.x > 0)
+            {
+                renderer.flipX = true;
+            }
+
+        }
+        else
+        {
+            if (direction.z > 0)
+            {
+                renderer.sprite = upSprite;
+            }
+            else if (direction.z < 0)
+            {
+                renderer.sprite = downSprite;
+            }
         }
     }
+        
 }
---
+++ LassMichInRuhGame/Assets/Kevin/Scripts/SanityController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SanityController : MonoBehaviour
+{
+    public static SanityController instance { get; private set; }
+    public float maxSanity = 100;
+    public UnityEvent OnInsane;
+    float sanity;
+
+    private void Awake()
+    {
+        instance = this;
+        sanity = maxSanity;
+    }
+
+    public void AddSanity(float amount)
+    {
+        sanity = Mathf.Max(sanity + amount, maxSanity);
+    }
+
+    public void RemoveSanity(float amount)
+    {
+        sanity -= amount;
+        if (sanity <= 0)
+        {
+            sanity = 0;
+            OnInsane.Invoke();
+        }
+    }
+
+    public void OnGUI()
+    {
+        GUILayout.Label($"Sanity: {sanity}");
+    }
+}
