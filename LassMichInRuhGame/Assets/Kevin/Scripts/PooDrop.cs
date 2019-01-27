0a611d4687c3ce9a66f812b8fe9bdd073496627f
Kevin Balz <kevin.balz@web.de>
2019-01-27 09:32:17 +0100

    Money drops

--- LassMichInRuhGame/Assets/Kevin/Scripts/PooDrop.cs
+++ LassMichInRuhGame/Assets/Kevin/Scripts/PooDrop.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class PooDrop : MonoBehaviour
 {
-    public float sanityDamage = 10;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            SanityController.instance.RemoveSanity(sanityDamage);
             other.GetComponent<MainCharacterController>().ApplyPoo();
             Destroy(gameObject);
         }
