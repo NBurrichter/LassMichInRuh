50686d6d96a49c88a15c32edbe7512cb03da0a54
Kevin Balz <kevin.balz@web.de>
2019-01-27 14:30:31 +0100

    Title screen

--- LassMichInRuhGame/Assets/Kevin/Scripts/RestartButton.cs
+++ LassMichInRuhGame/Assets/Kevin/Scripts/RestartButton.cs
@@ -8,7 +8,6 @@ public class RestartButton : MonoBehaviour
     public string scene;
     public void Restart()
     {
-        gameObject.SetActive(false);
         SceneManager.LoadSceneAsync(scene);
     }
 }
